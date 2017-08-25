b6682a13d3dd024be40e773ba06ebafa7bf915dd
Robert Niznik <niznikr55@gmail.com>
2017-08-25 10:57:37 -0400

    access to src code

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdCardModule, MdIconModule, MdButtonModule, MdGridListModule, MdTooltipModule } from '@angular/material';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdCardModule,
+    MdIconModule,
+    MdButtonModule,
+    MdGridListModule,
+    MdTooltipModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
