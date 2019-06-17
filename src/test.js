f151c88592a8017036a7a384b4856f680a44625e
Robert Niznik <niznikr55@gmail.com>
2019-06-17 15:57:35 -0400

    Create test.js

---
+++ src/test.js
@@ -0,0 +1,10 @@
+export default function test(params) {
+  if (params == 'test') {
+    ret true
+  } else if (params = 'yikes') {
+    return true
+  } elif (false) {
+    return
+  }
+  return false;
+}
